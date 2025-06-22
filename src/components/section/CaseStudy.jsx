8f21f7ddc41dc7f0dafbaad8a2e30a9c91172d8d
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2025-06-22 12:34:00 -0400

    add memory game

--- src/components/section/CaseStudy.jsx
+++ src/components/section/CaseStudy.jsx
@@ -1,5 +1,6 @@
 import comingSoonSign from "./../../assets/comingsoonsign.png";
 import serviceSite from "./../../assets/ServiceSite.png";
+import memoryGame from "./../../assets/MemoryGame.png";
 export const CaseStudy = () => {
   return (
     <section className="w-screen min-h-[80vh] p-5 space-y-5 flex flex-col  justify-center items-center gap-20">
@@ -10,8 +11,12 @@ export const CaseStudy = () => {
       <div className="carousel carousel-center bg-stone-100 rounded-box  max-w-70  min-[500px]:max-w-[85vw] space-x-4 p-4 gap-5 shadow-md">
         <div className="carousel-item caseStudyCards">
           <div className="card w-60 min-[832px]:w-96 shadow-md bg-white">
-            <figure>
-              <img src={serviceSite} alt="Coming soon image" />
+            <figure className="basis-1/2 h-full">
+              <img
+                src={serviceSite}
+                alt="Service Site Image"
+                className="object-fill"
+              />
             </figure>
             <div className="card-body">
               <h2 className="card-title">Design 1: Service site</h2>
@@ -45,26 +50,37 @@ export const CaseStudy = () => {
 
         <div className="carousel-item caseStudyCards">
           <div className="card w-60 min-[832px]:w-96 shadow-md bg-white">
-            <figure>
-              <img src={comingSoonSign} alt="Coming soon image" />
+            <figure className="basis-1/2 h-full">
+              <img
+                src={memoryGame}
+                alt="Memory Game Screenshot"
+                className="object-fill"
+              />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Design 2: A small game</h2>
-              <p>Description of this case study. A small game of some kind.</p>
+              <h2 className="card-title">Design 2: Swapped! A Memory Game</h2>
+              <p>
+                Cup flipping memory game that tests your sharpness and mind
+                abilities. The game includes a unique twist where cups will also
+                swap positions at the end of the user’s turn.
+              </p>
               <div className="card-actions justify-end">
-                <div className="badge badge-outline">React/JavaScript</div>
-                <div className="badge badge-outline">HTML/CSS</div>
+                <div className="badge badge-outline">NextJS</div>
+                <div className="badge badge-outline">TailwindCSS/DaisyUI</div>
+                <div className="badge badge-outline">Figma</div>
+                <div className="badge badge-outline">FontAwesome</div>
+                <div className="badge badge-outline">Motion Library</div>
+                <div className="badge badge-outline">Git & CI/CD</div>
               </div>
 
               <div className="pt-2 flex">
-                <button
-                  className="caseStudyLinks"
-                  onClick={() =>
-                    document.getElementById("my_modal_2").showModal()
-                  }
+                <a
+                  href="https://swapped-game.netlify.app/"
+                  target="_blank"
+                  className="caseStudyLinks font-bold"
                 >
-                  <p className="font-bold">View case study →</p>
-                </button>
+                  View case study →
+                </a>
               </div>
             </div>
           </div>
@@ -72,7 +88,7 @@ export const CaseStudy = () => {
 
         <div className="carousel-item caseStudyCards">
           <div className="card w-60 min-[832px]:w-96 shadow-md bg-white">
-            <figure>
+            <figure className="basis-1/2 h-full">
               <img src={comingSoonSign} alt="Coming soon image" />
             </figure>
             <div className="card-body">
@@ -102,7 +118,7 @@ export const CaseStudy = () => {
 
         <div className="carousel-item caseStudyCards">
           <div className="card w-60 min-[832px]:w-96 shadow-md bg-white">
-            <figure>
+            <figure className="basis-1/2 h-full">
               <img src={comingSoonSign} alt="Coming soon image" />
             </figure>
             <div className="card-body">
